e6cd4ceec0a3a84f383432ae0200d23fcb6e842f
SumeyraAltinisik <sfa2023@gmail.com>
2023-01-16 20:01:07 +0300

    Add project files.

---
+++ NWProductService/Models/Category.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+
+namespace NWProductService.Models;
+
+public partial class Category
+{
+    public int CategoryId { get; set; }
+
+    public string CategoryName { get; set; } = null!;
+
+    public string? Description { get; set; }
+
+    //public byte[]? Picture { get; set; }
+    [JsonProperty(NullValueHandling = NullValueHandling.Ignore)]
+
+    public virtual ICollection<Product> Products { get; } = new List<Product>();
+}
---
+++ NWProductService/Models/UrunFiyatGecmisi.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace NWProductService.Models;
+
+public partial class UrunFiyatGecmisi
+{
+    public int Id { get; set; }
+
+    public int? ProductsId { get; set; }
+
+    public decimal? EskiFiyat { get; set; }
+
+    public decimal? YeniFiyat { get; set; }
+
+    public DateTime? DegistirmeTarihi { get; set; }
+}

28e577c47417b8a82b47767a84d31c516630e04d
SumeyraAltinisik <sfa2023@gmail.com>
2023-01-17 19:57:27 +0300

    json

---
+++ NWTest/Program.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+namespace NWTest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Categories().ConfigureAwait(true);
+        //http://localhost:5093/
+            Console.WriteLine("Hello, World!");
+        }
+        static async Task Categories()
+        {
+            using (var httpClient = new HttpClient())
+            {
+                using(var response = await httpClient.GetAsync("http://localhost:5093/api/satis/Product/GetCategories"))
+                {
+                    string apiResponse = await response.Content.ReadAsStringAsync();
+                    var listem = JsonConvert.DeserializeObject(apiResponse);
+                }
+            }
+        }
+
+    }
+}
